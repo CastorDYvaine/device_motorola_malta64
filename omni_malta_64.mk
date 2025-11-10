35475449002c8f3ad762ea057feb0df3bf907fe1
CastorDYvaine <88608105+CastorDYvaine@users.noreply.github.com>
2025-11-10 23:07:10 +1300

    Update PRODUCT_NAME to match device name

--- omni_malta_64.mk
+++ omni_malta_64.mk
@@ -9,14 +9,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from malta_64 device
 $(call inherit-product, device/motorola/malta_64/device.mk)
 
 PRODUCT_DEVICE := malta_64
-PRODUCT_NAME := omni_malta_64
+PRODUCT_NAME := malta_64
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e(7)
 PRODUCT_MANUFACTURER := motorola
